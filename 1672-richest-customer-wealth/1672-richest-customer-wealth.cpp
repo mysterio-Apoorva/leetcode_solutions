3a90ca6ac0293a2876390b96d1a16d888ba9b39e
Apoorva Kumar Jha <apoorvakumarjha_cse_25a01051@dtu.ac.in>
2026-06-22 11:03:26 +0530

    Time: 0 ms (100%), Space: 11.3 MB (81.69%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max = 0;
+        int m = accounts.size();
+        int n = accounts[0].size();
+        for (int i = 0; i < m; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum = sum + accounts[i][j];
+            }
+            if (max <= sum)
+                max = sum;
+        }
+        return max;
+    }
+};
