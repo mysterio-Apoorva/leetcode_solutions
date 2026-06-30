6bee6b114c8e90eb7ae4deea00f310e3e8dd3637
Apoorva Kumar Jha <apoorvakumarjha_cse_25a01051@dtu.ac.in>
2026-06-30 21:38:44 +0530

    Time: 0 ms (100%), Space: 7.9 MB (41.17%) - LeetHub

---
+++ 0754-reach-a-number/0754-reach-a-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        target = abs(target);
+
+        long long sum = 0;
+        int step = 0;
+
+        while (sum < target || (sum - target) % 2 != 0) {
+            step++;
+            sum += step;
+        }
+
+        return step;
+    }
+};
