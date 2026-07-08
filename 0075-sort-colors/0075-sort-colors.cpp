8a4f68b926c094f19d32bc4a1441bb64e1f1349a
Apoorva Kumar Jha <apoorvakumarjha_cse_25a01051@dtu.ac.in>
2026-07-08 23:35:33 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int min;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            min = i;
+            for (int j = i; j < nums.size(); j++) {
+                if (nums[j] <= nums[min])
+                    min = j;
+            }
+            swap(nums[i], nums[min]);
+        }
+    }
+};
