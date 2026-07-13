2b4c899685dbc1d69cf2a5a4f996a49f8be81239
Apoorva Kumar Jha <apoorvakumarjha_cse_25a01051@dtu.ac.in>
2026-07-13 19:56:08 +0530

    Time: 0 ms (100%), Space: 8.8 MB (92.84%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int cnt = 0;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] != ' ')
+                cnt++;
+            if (s[i] == ' ') {
+                if (cnt == 0)
+                    continue;
+                else
+                    break;
+            }
+        }
+        return cnt;
+    }
+};
