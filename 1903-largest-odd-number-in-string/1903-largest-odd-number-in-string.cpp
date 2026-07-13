b814c9f2d35152e61e6c68039b5009ba64ed2cdd
Apoorva Kumar Jha <apoorvakumarjha_cse_25a01051@dtu.ac.in>
2026-07-14 00:10:20 +0530

    Time: 0 ms (100%), Space: 16.8 MB (92.76%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        while (!num.empty()) {
+            if ((num.back() - '0') % 2 != 0)
+                return num;
+
+            num.pop_back();
+        }
+
+        return "";
+    }
+};
