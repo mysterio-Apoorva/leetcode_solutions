d56a609464d13d11ce0f3e791ac24583afa23faf
Apoorva Kumar Jha <apoorvakumarjha_cse_25a01051@dtu.ac.in>
2026-07-18 23:00:16 +0530

    Time: 30 ms (28.11%), Space: 81.2 MB (79.64%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        heights.push_back(0);   // Sentinel
+
+        int ans = 0;
+
+        for (int i = 0; i < heights.size(); i++) {
+
+            while (!st.empty() && heights[st.top()] > heights[i]) {
+
+                int height = heights[st.top()];
+                st.pop();
+
+                int width;
+
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.top() - 1;
+
+                ans = max(ans, height * width);
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
