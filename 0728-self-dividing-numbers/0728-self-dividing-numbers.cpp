4571b088ddeb50f4395b70947de0ce953010b504
Apoorva Kumar Jha <apoorvakumarjha_cse_25a01051@dtu.ac.in>
2026-07-31 23:40:36 +0530

    Time: 0 ms (100%), Space: 9.2 MB (61.8%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isSelfDividing(int num) {
+        int temp = num;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+
+            if (digit == 0 || num % digit != 0)
+                return false;
+
+            temp /= 10;
+        }
+
+        return true;
+    }
+
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+
+        for (int i = left; i <= right; i++) {
+            if (isSelfDividing(i))
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
