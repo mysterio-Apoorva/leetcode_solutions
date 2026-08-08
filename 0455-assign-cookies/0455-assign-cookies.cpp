ec5cf8e0a143d83882dc861b69b0db5591e4b98f
Apoorva Kumar Jha <apoorvakumarjha_cse_25a01051@dtu.ac.in>
2026-08-09 01:32:25 +0530

    Time: 9 ms (49.8%), Space: 44.7 MB (97.93%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int l = 0;
+        int r = 0;
+        int count = 0;
+        while (l < g.size() && r < s.size()) {
+            if (g[l] <= s[r]) {
+                count++;
+                l++;
+                r++;
+            }
+            else
+            r++;
+        }
+        return count;
+    }
+};
