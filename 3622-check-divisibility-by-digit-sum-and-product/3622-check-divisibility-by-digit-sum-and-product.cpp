9f37098849f802adb2cbc03588556509e8f2d6ac
Apoorva Kumar Jha <apoorvakumarjha_cse_25a01051@dtu.ac.in>
2026-08-22 23:54:54 +0530

    Time: 0 ms (100%), Space: 7.7 MB (68.55%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int original = n;
+        int digitSum = 0;
+        int digitProduct = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+            digitSum += digit;
+            digitProduct *= digit;
+            n /= 10;
+        }
+
+        return original % (digitSum + digitProduct) == 0;
+    }
+};
