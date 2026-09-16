215c4d4e6755462ec9c7db969a11d779dccbad58
Apoorva Kumar Jha <apoorvakumarjha_cse_25a01051@dtu.ac.in>
2026-09-17 00:49:15 +0530

    Time: 0 ms (100%), Space: 87.4 MB (10.78%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int i = 0; i < bills.size(); i++) {
+
+            if (bills[i] == 5) {
+                five++;
+            }
+
+            else if (bills[i] == 10) {
+                if (five < 1)
+                    return false;
+
+                five--;
+                ten++;
+            }
+
+            else if (bills[i] == 20) {
+
+                // Prefer giving one 10 and one 5
+                if (ten >= 1 && five >= 1) {
+                    ten--;
+                    five--;
+                }
+
+                // Otherwise give three 5s
+                else if (five >= 3) {
+                    five -= 3;
+                }
+
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
